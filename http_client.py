54ddb27bc5be74248b47e826c5f6b61b3bc581f6
jmurugan-fzj <j.murugan@fz-juelich.de>
2024-03-19 16:18:45 +0100

    - Copied the files from pasta-eln repo and adjusted the files for the independent run.

--- http_client.py
+++ http_client.py
@@ -8,11 +8,99 @@
 #
 #  You should have received a copy of the license with this file. Please refer the license file for more information.
 import logging
-from typing import Any, Union
+from asyncio import CancelledError, IncompleteReadError, InvalidStateError, LimitOverrunError, \
+  TimeoutError as AsyncTimeoutError
+from functools import wraps
+from json import JSONDecodeError
+from typing import Any, Callable
+from typing import Union
 
 from aiohttp import BasicAuth, ClientResponse, ClientSession
+from aiohttp import ClientConnectorError, InvalidURL
 
-from pasta_eln.utils import handle_http_client_exception
+
+def handle_http_client_exception(wrapped: Callable[..., Any]) -> Callable[..., Any]:
+  """
+  Handle exceptions from http client methods.
+  Args:
+    wrapped (Callable): The wrapped function where the exceptions can be raised.
+
+  Returns:
+    The wrapper function which handles the exceptions.
+  """
+
+  @wraps(wrapped)
+  async def wrapper(self: Any, *args: object, **kwargs: object) -> Any:
+    try:
+      return await wrapped(self, *args, **kwargs)
+    except AsyncTimeoutError as e:
+      url = kwargs["base_url"] if 'base_url' in kwargs else args[0]
+      error = f"Client session request timeout for url ({url}) with error: {e}"
+      self.logger.error(error)
+      self.session_request_errors.append(error)
+      return {}
+    except CancelledError as e:
+      url = kwargs["base_url"] if 'base_url' in kwargs else args[0]
+      error = f"Client session request cancelled for url ({url}) with error: {e}"
+      self.logger.error(error)
+      self.session_request_errors.append(error)
+      return {}
+    except InvalidStateError as e:
+      url = kwargs["base_url"] if 'base_url' in kwargs else args[0]
+      error = f"Client session request in invalid state for url ({url}) with error: {e}"
+      self.logger.error(error)
+      self.session_request_errors.append(error)
+      return {}
+    except IncompleteReadError as e:
+      url = kwargs["base_url"] if 'base_url' in kwargs else args[0]
+      error = f"Client session request incomplete read for url ({url}) with error: {e}"
+      self.logger.error(error)
+      self.session_request_errors.append(error)
+      return {}
+    except LimitOverrunError as e:
+      url = kwargs["base_url"] if 'base_url' in kwargs else args[0]
+      error = f"Client session request limit overrun for url ({url}) with error: {e}"
+      self.logger.error(error)
+      self.session_request_errors.append(error)
+      return {}
+    except TypeError as e:
+      url = kwargs["base_url"] if 'base_url' in kwargs else args[0]
+      error = f"Client session type error for url ({url}) with error: {e}"
+      self.logger.error(error)
+      self.session_request_errors.append(error)
+      return {}
+    except ClientConnectorError as e:
+      url = kwargs["base_url"] if 'base_url' in kwargs else args[0]
+      error = f"ClientConnectorError for url ({url}) with error: {e}"
+      self.logger.error(error)
+      self.session_request_errors.append(error)
+      return {}
+    except InvalidURL as e:
+      url = kwargs["base_url"] if 'base_url' in kwargs else args[0]
+      error = f"Client session InvalidURL for url ({url}) with error: {e}"
+      self.logger.error(error)
+      self.session_request_errors.append(error)
+      return {}
+    except JSONDecodeError as e:
+      url = kwargs["base_url"] if 'base_url' in kwargs else args[0]
+      error = f"Client session JSONDecodeError for url ({url}) with error: {e}"
+      self.logger.error(error)
+      self.session_request_errors.append(error)
+      return {}
+    except AssertionError as e:
+      url = kwargs["base_url"] if 'base_url' in kwargs else args[0]
+      error = f"Client session AssertionError for url ({url}) with error: {e}"
+      self.logger.error(error)
+      self.session_request_errors.append(error)
+      return {}
+    except ValueError as e:
+      url = kwargs["base_url"] if 'base_url' in kwargs else args[0]
+      error = f"Client session ValueError for url ({url}) with error: {e}"
+      self.logger.error(error)
+      self.session_request_errors.append(error)
+      return {}
+
+  return wrapper
 
 
 async def prepare_result(response: ClientResponse) -> dict[str, Any]:
